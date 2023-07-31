7270b8f33023ccc12fff70cd1222a8164017f14f
HeyLet <leticiafaleiros9@gmail.com>
2023-07-31 11:04:24 -0300

    inicio do projeto

---
+++ src/components/Billing.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+// import styles, { layout } from '../style'
+
+const Billing = () => {
+  return (
+    <section>
+      <div></div>
+    </section>
+  )
+}
+
+export default Billing
